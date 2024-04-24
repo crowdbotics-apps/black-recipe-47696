7d51be476dd7b63aa56d8ba04ecc37d3ed572b31
Crowdbotics <team@crowdbotics.com>
2024-04-24 15:36:31 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-recipe-47696",
+  url: "https://black-recipe-47696.botics.co",
+  api: "https://black-recipe-47696.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
